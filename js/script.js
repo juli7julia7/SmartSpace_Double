f616caaf4ee07d8fb6d97d42fbe9d59e7532d2f0
juli7julia7 <106538206+juli7julia7@users.noreply.github.com>
2023-10-12 17:43:53 +0300

    add js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.slider-hero', {
+   loop: true,
+
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.slider-hero__arrow.swiper-button-next',
+      prevEl: '.slider-hero__arrow.swiper-button-prev',
+   },
+
+});
